3369fded1da5576e523f4296c099172da71734f2
Nurmuhammad <ashuraliyevnurmuhammad16@gmail.com>
2026-01-24 02:08:45 +0500

    update prisma.schema and seeder

--- src/common/config/config.module.ts
+++ src/common/config/config.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
 import { RedisModule } from "./redis/redis.module";
+import { JwtModules } from "./jwt/jwt.module";
 
 @Module({
-    imports: [JwtModule, RedisModule,JwtModule],
+    imports: [ JwtModules,RedisModule,JwtModule],
     controllers: [],
     providers: []
 })
--- src/common/config/redis/redis.service.ts
+++ src/common/config/redis/redis.service.ts
@@ -3,40 +3,40 @@ import Redis from 'ioredis';
 
 @Injectable()
 export class RedisService implements OnModuleInit, OnModuleDestroy {
-    private redis_client: Redis;
+  private redis_client: Redis;
 
-    async onModuleInit() {
-        this.redis_client = new Redis({
-            host: process.env.REDIS_HOST || 'localhost',
-            port: Number(process.env.REDIS_PORT) || 6379,
-        });
+  async onModuleInit() {
+    this.redis_client = new Redis({
+      host: process.env.REDIS_HOST || 'localhost' || 'redis',
+      port: Number(process.env.REDIS_PORT) || 6379,
+    });
 
-        this.redis_client.on('connect', () => {
-            console.log('✅ Redis connected');
-        });
+    this.redis_client.on('connect', () => {
+      console.log('✅ Redis connected');
+    });
 
-        this.redis_client.on('error', (err) => {
-            console.error('❌ Redis error:', err);
-        });
+    this.redis_client.on('error', (err) => {
+      console.error('❌ Redis error:', err);
+    });
+  }
+
+  async set(key: string, value: string, ttlSeconds?: number) {
+    if (ttlSeconds) {
+      await this.redis_client.set(key, value, 'EX', ttlSeconds);
+    } else {
+      await this.redis_client.set(key, value);
     }
+  }
 
-    async set(key: string, value: string, ttlSeconds?: number) {
-        if (ttlSeconds) {
-            await this.redis_client.set(key, value, 'EX', ttlSeconds);
-        } else {
-            await this.redis_client.set(key, value);
-        }
-    }
+  async get(key: string): Promise<string | null> {
+    return this.redis_client.get(key);
+  }
 
-    async get(key: string): Promise<string | null> {
-        return this.redis_client.get(key);
-    }
+  async del(key: string) {
+    return this.redis_client.del(key);
+  }
 
-    async del(key: string) {
-        return this.redis_client.del(key);
-    }
-
-    async onModuleDestroy() {
-        await this.redis_client.quit();
-    }
+  async onModuleDestroy() {
+    await this.redis_client.quit();
+  }
 }
