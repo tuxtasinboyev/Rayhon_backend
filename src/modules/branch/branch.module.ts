49ba3e0965540cf9b6f88a66acf342fa49e4ae0e
Tuxtasinboyev Omadbek <tuxtasinboyev@kali.kali>
2026-01-24 02:02:05 +0500

    tugrlandi

--- src/modules/branch/branch.module.ts
+++ src/modules/branch/branch.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BranchService } from './branch.service';
 import { BranchController } from './branch.controller';
+import { PrismaModule } from 'src/common/database/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports:[PrismaModule,ConfigModule],
   providers: [BranchService],
   controllers: [BranchController]
 })
