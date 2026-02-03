c95edacf448eef10b7d497a4882fca580f800d98
Tuxtasinboyev Omadbek <tuxtasinboyev@kali.kali>
2026-02-03 16:11:26 +0500

    tugirlandi

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*'
+  })
 
   app.useGlobalPipes(
     new ValidationPipe({
